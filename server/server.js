b26e012638a5b050f7f21afd534fe8b1fdf92313
Manav Bokinala <manav.bokinala@gmail.com>
2017-10-27 18:41:46 -0700

    Added home page

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+	res.send('Hi');
+})
 
 app.listen(3000, () => {
 	console.log('Server started on port 3000');
